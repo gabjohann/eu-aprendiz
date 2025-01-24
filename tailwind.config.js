2c693b1bb415cf43ee4d9c9422569db021e7ed11
Lucas Gabriel <gabif.johann@gmail.com>
2025-01-24 18:26:21 -0300

    add: color tokens and image

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.tsx'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				lightBlue: '#02007B',
+				darkBlue: '#0B0B32',
+				orange: '#ED921F',
+			},
+			backgroundImage: {
+				videoCard: "url('./src/assets/bg-card.png')",
+			},
+		},
 	},
 	plugins: [],
 }
