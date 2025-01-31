e7be6c4ee200c8316eca03a2b96ec015ef461d27
Lucas Gabriel <gabif.johann@gmail.com>
2025-01-31 10:35:48 -0300

    add: video card component

---
+++ src/components/video-card.tsx
@@ -0,0 +1,22 @@
+type VideoCardProps = {
+	title: string
+	link: string
+}
+
+export function VideoCard({ title, link }: VideoCardProps) {
+	return (
+		<div className="flex flex-col items-center w-40 h-auto px-4 pt-12 pb-8 bg-videoCard lg:w-52">
+			<p className="pb-16 text-base text-center uppercase text-orange">
+				{title}
+			</p>
+			<button
+				type="button"
+				className="px-6 py-3 text-xs text-white uppercase rounded bg-orange orangeButton"
+			>
+				<a href={link} target="_blank" rel="noreferrer">
+					Saiba mais
+				</a>
+			</button>
+		</div>
+	)
+}
