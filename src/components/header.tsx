4ae4d284f8fccad71e30a7e2ed1d49b0dac22a23
Lucas Gabriel <gabif.johann@gmail.com>
2025-01-31 10:35:31 -0300

    add: header component

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+	return (
+		<header>
+			<nav className="bg-darkBlue py-4">
+				<ul className="flex items-center uppercase text-xs gap-5 justify-center text-white">
+					<li>a empresa</li>
+					<li>serviços</li>
+					<li>vídeos</li>
+					<li>contato</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
