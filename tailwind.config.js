887bc3870c1aab58e2a7039fd79e689478b8c8d5
Lucas Gabriel <gabif.johann@gmail.com>
2025-01-31 10:34:53 -0300

    chore: radix style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const { violet, blackA, mauve, green, gray } = require('@radix-ui/colors')
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.tsx'],
@@ -7,9 +8,31 @@ export default {
 				lightBlue: '#02007B',
 				darkBlue: '#0B0B32',
 				orange: '#ED921F',
+				...mauve,
+				...violet,
+				...green,
+				...blackA,
+				...gray,
 			},
 			backgroundImage: {
 				videoCard: "url('./src/assets/bg-card.png')",
+			},
+			keyframes: {
+				overlayShow: {
+					from: { opacity: '0' },
+					to: { opacity: '1' },
+				},
+				contentShow: {
+					from: {
+						opacity: '0',
+						transform: 'translate(-50%, -48%) scale(0.96)',
+					},
+					to: { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+				},
+			},
+			animation: {
+				overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+				contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
 			},
 		},
 	},
